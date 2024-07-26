dc378e1ada643512ed95f796a4008adb8ca28bc2
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-07-26 17:58:43 +0530

    Curd Operation -Create Rest api

--- DAY8/index.js
+++ DAY8/index.js
@@ -1,12 +1,68 @@
 const express = require('express')
-const { studentRouter } = require('./routes/router.student')
-const { teacherRouter } = require('./routes/router.teacher')
+const fs = require('fs');
+const students = require('./data/student.json')
 const app = express();
 
-app.use('/student', studentRouter)
-app.use('/teacher', teacherRouter)
+// middleware
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('This is the Home page')
+})
+app.get('/api/student', (req, res) => {
+
+    res.json(students)
+})
+app.get('/api/student/:id', (req, res) => {
+    console.log(req.params)
+    const _id = req.params.id
+    const student = students.find((std) => std.id == _id)
+    res.json(student);
+})
+
+
+app.post('/api/student', (req, res) => {
+    const lastId = students[students.length - 1].id + 1;
+    // const newStudent = {
+    //     id: lastId,
+    //     ...req.body
+    // }
+    const newStudent = Object.assign({ id: lastId }, req.body)
+    students.push(newStudent)
+    res.send(newStudent)
+    console.log(students)
+
+    fs.writeFile('./data/student.json', JSON.stringify(students), error => {
+        if (error) {
+            console.log(error)
+        }
+    })
+})
+app.patch('/api/student/:id', (req, res) => {
+    const _id = req.params.id;
+    const student = students.find((std) => std.id == _id);
+    const index = students.indexOf(student)
+    const updateStudent = Object.assign(student, req.body);
+    students[index] = updateStudent
+    fs.writeFile('./data/student.json', JSON.stringify(students), error => {
+        if (error) {
+            console.log(error)
+        }
+    })
+    res.send('User Updated')
+})
+app.delete('/api/student/:id',(req,res)=>{
+    const _id = req.params.id;
+    const student = students.find((std) => std.id == _id);
+    const index = students.indexOf(student)
+    students.splice(index,1);
+    fs.writeFile('./data/student.json', JSON.stringify(students), error => {
+        if (error) {
+            console.log(error)
+        }
+    })
+    res.send('User Updated')
+
 })
 
 
