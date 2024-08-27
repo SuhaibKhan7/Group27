9a43649e13d1638d8e929fbdb24652fd0c943e51
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-08-27 19:15:26 +0530

    GITGHUB AUTH

--- DAY14/index.js
+++ DAY14/index.js
@@ -1,42 +1,61 @@
 // const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const express = require('express');
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
- 
 const app = express();
+
+app.set('view engine',"ejs")
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/auth/github', async (req, res) => {
-    const code = req.query.code; // Get the 'code' parameter from the query string
+    const { code } = req.query; // Get the 'code' parameter from the query string
     console.log("Authorization Code:", code);
 
     try {
         const tokenResponse = await fetch('https://github.com/login/oauth/access_token', {
             method: "POST",
             headers: {
-                "Content-Type": "application/json",
+                "Content-type": "application/json",
                 Accept: "application/json"
             },
             body: JSON.stringify({
-                client_id: 'Ov23liVth6nMARWLmzwI',
-                client_secret: 'ffd072d378a3f48d067218f95c2d2f364fb4c7ed',
-                code: code
+                client_id: 'Ov23liLYCAQEuWMdzVdh',
+                client_secret: 'aefbe3ca709ea8223356baa0a97d4fe59db74deb',
+                code: code,
+                redirect_uri: 'http://localhost:4000/auth/github'
             })
         });
+        const token = await tokenResponse.json()
+        console.log(token)
 
-        const token = await tokenResponse.json();
-        console.log("Access Token:", token);
+        const userdetails = await fetch('https://api.github.com/user', {
+            headers: {
+                Authorization: `Bearer ${token.access_token} `
+            }
+        })
 
-        res.send("Token received. Check console for details.");
+        console.log(userdetails)
+        const userinfo = await userdetails.json()
+        console.log(userinfo)
+        res.redirect('/dashboard')
+ 
+
+
     } catch (error) {
         console.error("Error fetching token:", error);
         res.status(500).send("Error during authentication");
     }
+
+    res.send("Sign....")
+
 });
 
-
+app.get('/dashboard', (req, res) => {
+    res.send("This is the Dashboard")
+})
 
 
 
